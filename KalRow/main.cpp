a880c11ce1d09745c1fc255c30ab47bda1fee88e
birdman98 <piotrmatras21@gmail.com>
2016-05-15 00:34:17 +0200

    dodanie liczenia równania liniowego

--- KalRow/main.cpp
+++ KalRow/main.cpp
@@ -4,8 +4,16 @@
 
 using namespace std;
 
-int main() {
+string s;
 
+int main() 
+{
+    cout<<"podaj rownanie do policzenia: \n" ;
+    getline(cin,s) ;
+
+    cout<<rownanie_liniowe(s) ;
+
+return 0 ;
 	
 	_getch();
 }
