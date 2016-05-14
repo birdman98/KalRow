4565d1ff98ffb412d991523723136dfc46b9f647
birdman98 <piotrmatras21@gmail.com>
2016-05-15 00:18:54 +0200

    dodanie funkcji do liczenia równań liniowych

--- KalRow/Funkcje.cpp
+++ KalRow/Funkcje.cpp
@@ -6,3 +6,23 @@ float delta(float a, float b, float c) {
 	float wynik = pow(b, 2) - 4 * a * c;
 	return wynik;
 }
+
+float rownanie_liniowe (string s)
+{
+    float a=0, b=0 ;
+    string a_str ;
+
+    for (int i=0; i<s.length(); i++)
+    {
+        if (s[i]=='x')
+        {
+             a_str=s.substr(0,i) ;
+             s.erase(0,i+1) ;
+        }
+
+    }
+     b=atoi(s.c_str()) ;
+     a=atoi(a_str.c_str()) ;
+
+return -b/a ;
+}
