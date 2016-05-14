7bc1c8d811d37544c3ec9cb21ff3da9c420e2e0b
Slajni <esinnley@gmail.com>
2016-05-14 15:10:35 +0200

    Dodanie funkcji liczącej delte
    JW.

--- KalRow/main.cpp
+++ KalRow/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <conio.h>
+#include <math.h>
 
 using namespace std;
+
+float delta(float a, float b, float c);
 
 int main() {
 
 	
 	_getch();
+}
+
+float delta(float a, float b, float c) {
+
+	float wynik = pow(b, 2) - 4 * a * c;
+	return wynik;
 }
