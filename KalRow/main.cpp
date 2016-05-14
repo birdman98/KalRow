1e0ad4779194358076b870a967a5b36b78f8ae16
Slajni <esinnley@gmail.com>
2016-05-14 14:29:04 +0200

    Update main.cpp

--- KalRow/main.cpp
+++ KalRow/main.cpp
@@ -1,26 +1,10 @@
 #include <iostream>
 #include <conio.h>
-#include <math.h>
 
 using namespace std;
 
-
-float Delta(float a, float b, float c);
-
 int main() {
 
-	cout << Delta(4, 4, 1);
 	
-
-
 	_getch();
-}
-float Delta(float a, float b, float c) {
-
-	float wynik;
-
-	wynik = pow(b, 2.0) - 4 * a * c;
-
-
-	return wynik;
 }
